fd67c6549087f873d92b5848b58a2b41b9ac5b38
Lucas L. Lima <lucaslopes.developer@gmail.com>
2024-04-09 23:52:17 -0300

    Update sideBarAnimation.js

--- sideBarAnimation.js
+++ sideBarAnimation.js
@@ -1,26 +1,26 @@
 // Seleciona todos os elementos da página
-const elementos = document.querySelectorAll('.sideBar__animation a');
+const elementos = document.querySelectorAll('.sideBar__animation a')
 console.log(elementos)
 // Adiciona um event listener de clique a cada elemento
 elementos.forEach(elemento => {
     elemento.addEventListener('click', function(event) {
         // Loga o elemento clicado no console
         event.preventDefault()
-        console.log('Elemento clicado:', event.target);
+        console.log('Elemento clicado:', event.target)
 
         
-        let elementoAnterior = event.target.previousElementSibling;
-        let elementoPosterior = event.target.nextElementSibling;
+        let elementoAnterior = event.target.previousElementSibling
+        let elementoPosterior = event.target.nextElementSibling
         resetClasses()
         this.classList.add('sideBar__animation-active')
         console.log(elementoAnterior, elementoPosterior)
 
-        /* if(event.target.previousElementSibling){
+        if(event.target.previousElementSibling){
             elementoAnterior.classList.add('previous-element')    
         }
-        if(event.target.nextElementSibling;){
+        if(event.target.nextElementSibling){
             elementoPosterior.classList.add('next-element')    
-        } */
+        }
         
         
 
